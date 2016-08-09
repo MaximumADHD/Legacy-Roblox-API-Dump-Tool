22605e47d1476c45739c8cc4d8da2e82be0bae0e
Max <MPG1019@aol.com>
2016-08-09 00:08:40 -0500

    Initial Push

---
+++ src/APIDumpTool/APIDump.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace APIDumpTool
+{
+    partial class APIDump
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(APIDump));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "gametest1.robloxlabs",
+            "gametest2.robloxlabs",
+            "gametest3.robloxlabs",
+            "gametest4.robloxlabs",
+            "gametest5.robloxlabs",
+            "roblox"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.comboBox1.Size = new System.Drawing.Size(208, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.17323F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Database:";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(15, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "View API Dump";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(15, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Compare to Production";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(15, 146);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            this.status.TabIndex = 4;
+            // 
+            // APIDump
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 170);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "APIDump";
+            this.Text = "API Dump Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label status;
+
+    }
+}
+
---
+++ src/APIDumpTool/APIDump.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using LuaInterface;
+
+namespace APIDumpTool
+{
+
+    public partial class APIDump : Form
+    {
+        private WebClient http = new WebClient();
+
+        public APIDump()
+        {
+            InitializeComponent();
+        }
+
+        public async Task setStatus(string statusText = "")
+        {
+            if (!statusText.Equals(""))
+            {
+                statusText += "...";
+            }
+            status.Text = statusText;
+            await Task.Delay(100);
+        }
+
+        public void createDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+
+        public async Task extractRobloxZip(string database, string version, string fileName, string directory)
+        {
+            WebClient localHttp = new WebClient();
+            string url = database + version + "-" + fileName;
+            await setStatus("Fetching " + fileName);
+            byte[] file = localHttp.DownloadData(url);
+            string filePath = Path.Combine(directory, fileName);
+            await setStatus("Writing " + fileName);
+            FileStream writeFile = File.Create(filePath);
+            writeFile.Write(file, 0, file.Length);
+            writeFile.Close();
+            ZipArchive archive = ZipFile.Open(filePath,ZipArchiveMode.Read);
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                string entryName = entry.FullName;
+                await setStatus("Extracting " + entryName);
+                string relativePath = Path.Combine(directory,entryName);
+                if (!File.Exists(relativePath))
+                {
+                    entry.ExtractToFile(relativePath);
+                }
+            }
+        }
+
+        public async Task<string> loadRobloxBuild(string database, string version, string directory)
+        {
+            await setStatus("Building directory for " + version);
+            string directoryPath = Path.Combine(directory, version);
+            createDirectory(directoryPath);
+            // There are two directories that need to exist in order to get the application to cooperate.
+            // content, and PlatformContent/pc
+            string content = Path.Combine(directoryPath, "content");
+            createDirectory(content);
+            string platformContent = Path.Combine(directoryPath, "PlatformContent", "pc");
+            createDirectory(platformContent);
+            // Write AppSettings (required to get RobloxPlayerBeta to cooperate)
+            string appSettings = Path.Combine(directoryPath, "AppSettings.xml");
+            File.WriteAllText(appSettings, "<Settings><ContentFolder>content</ContentFolder><BaseUrl>http://www.roblox.com</BaseUrl></Settings>");
+            // Extract RobloxApp.zip and Libraries.zip from ROBLOX's setup cdn into our directory.
+            await extractRobloxZip(database, version, "RobloxApp.zip", directoryPath);
+            await extractRobloxZip(database, version, "Libraries.zip", directoryPath);
+            // Return the path to RobloxPlayerBeta.exe
+            return Path.Combine(directoryPath, "RobloxPlayerBeta.exe");
+        }
+
+        public async Task<string> getAPIFile(string database = "")
+        {
+            this.Enabled = false;
+            if (database.Equals(""))
+            {
+                database = comboBox1.Text;
+            }
+            database = "http://setup." + database + ".com/";
+            Console.WriteLine(database);
+            string version = http.DownloadString(database + "version.txt");
+            string apiDumps = Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"), "Roblox API Dumps");
+            createDirectory(apiDumps);
+            string filePath = Path.Combine(apiDumps, version) + ".txt";
+            if (!File.Exists(filePath))
+            {
+                string buildDir = Path.Combine(apiDumps,"Builds");
+                createDirectory(buildDir);
+                string robloxPlayerBeta = await loadRobloxBuild(database, version, buildDir);
+                Console.WriteLine(robloxPlayerBeta);
+                await setStatus("Extracting API Dump");
+                Process apiDump = Process.Start(robloxPlayerBeta, "--API api.txt");
+                apiDump.WaitForExit();
+                await setStatus("Writing API Dump");
+                string dumpPath = robloxPlayerBeta.Replace("RobloxPlayerBeta.exe", "api.txt");
+                File.Copy(dumpPath, filePath);
+            }
+            else
+            {
+                await setStatus("API Dump already generated!");
+            }
+            this.Enabled = true;
+            return filePath;
+        }
+
+        public string compareAPIDumps(string apiDump0, string apiDump1)
+        {
+            string result = "";
+            try
+            {
+                Lua parser = new Lua();
+                parser.DoString("PRODUCTION_API_DUMP = [[" + apiDump0 + "]]");
+                parser.DoString("GAMETEST_API_DUMP = [[" + apiDump1 + "]]");
+                string currentDir = Directory.GetCurrentDirectory();
+                string luaFilePath = Path.Combine(currentDir, "CompareToProduction.lua");
+                string luaFile = File.ReadAllText(luaFilePath);
+                parser.DoString(luaFile);
+                result = parser.GetString("FINAL_RESULT");
+            }
+            catch
+            {
+                result = "FAIL";
+                MessageBox.Show("Failed to compare API Dumps!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            return result;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = await getAPIFile();
+            Process.Start(filePath);
+            await setStatus();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            if (!comboBox1.Text.Equals("roblox"))
+            {
+                await setStatus("Getting gametest API Dump...");
+                string gametestBuild = File.ReadAllText(await getAPIFile());
+                await setStatus("Getting production API Dump...");
+                string robloxBuild = File.ReadAllText(await getAPIFile("roblox"));
+                await setStatus("Fetching results");
+                string result = compareAPIDumps(robloxBuild, gametestBuild);
+                if (result != "FAIL")
+                {
+                    if (result == "")
+                    {
+                        MessageBox.Show("No differences were found :(", "No results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            string apiDumps = Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"), "Roblox API Dumps");
+                            string file = Path.Combine(apiDumps, comboBox1.Text + "_diff.txt");
+                            File.WriteAllText(file, result);
+                            Process.Start(file);
+                        }
+                        catch
+                        {
+                            status.Text = "Couldn't write to file. Probably open in text editor.";
+                        }
+                    }
+                }
+                await setStatus("");
+            }
+            else
+            {
+                MessageBox.Show("You cannot compare the production API Dump to itself!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            this.Enabled = true;
+        }
+    }
+}
